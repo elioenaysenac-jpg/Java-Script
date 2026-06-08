334c95c9b1cf381b9e433dd3d41c8b8777b9c7e0
marina-arruda <elioenay.senac@gmail.com>
2026-06-08 16:24:20 -0400

    Adicionado operadores de atribuição

---
+++ operador-atribuicao-modulo.js
@@ -0,0 +1,7 @@
+let totalDoces = 10;
+let criancas = 3;
+
+// Queremos saber quantos doces sobrem após dividir igualmente
+totalDoces %= criancas; 
+
+console.log(totalDoces); // 1 -> pois 10 dividido por 3 sobra 1 
